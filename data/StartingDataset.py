dac93683bea42deeb15d8f98abea782feeb8ec40
NFJ1618 <67336232+NFJ1618@users.noreply.github.com>
2022-01-27 19:21:20 -0800

    Initial DataSet class written pre-test

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,4 +1,7 @@
 import torch
+import pandas as pd
+from PIL import Image
+from torchvision.transforms import ToTensor
 
 
 class StartingDataset(torch.utils.data.Dataset):
@@ -6,14 +9,25 @@ class StartingDataset(torch.utils.data.Dataset):
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
 
-    def __init__(self):
-        pass
+    def __init__(self, csv_path, train=True):
+        if train:
+            relative_path = "train_images/"
+        else:
+            relative_path = "test_images/"
+
+        df = pd.read_csv(csv_path)
+        self.image_ids = list(df['image_id'])
+        for id,i in enumerate(self.image_ids):
+            self.image_ids[i] = relative_path + id
+        self.labels = list(df["label"])
+        self.l = len(self.labels)
 
     def __getitem__(self, index):
-        inputs = torch.zeros([3, 224, 224])
-        label = 0
+        #inputs = torch.zeros([3, 224, 224])
+        image = Image(self.image_ids[index])
+        inputs = ToTensor(image)
 
-        return inputs, label
+        return inputs, self.labels[index]
 
-    def __len__(self):
-        return 10000
+    def __len__(self): #Assumes size of class doesn't change
+        return self.l 
