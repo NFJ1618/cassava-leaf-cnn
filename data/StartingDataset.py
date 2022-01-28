4c8b66d9cd74062871b5021463d156efa4f282a9
ellenwei <ellenwei@PersonalMicrosoftSoftware.ucla.edu>
2022-01-27 19:25:19 -0800

    Added comments, ignore

--- data/StartingDataset.py
+++ data/StartingDataset.py
@@ -1,17 +1,27 @@
 import torch
-
+from PIL import Image
+import torchvision.transforms as transforms
+import pandas as pd
 
 class StartingDataset(torch.utils.data.Dataset):
     """
     Dataset that contains 100000 3x224x224 black images (all zeros).
     """
-
-    def __init__(self):
-        pass
+    paths = []
+    
+    def __init__(self, csv_path):
+        data = pd.read_csv(csv_path)
+        self.image_ids = list(data[image_id])
+        self.labels = list(data[label])
+        
 
     def __getitem__(self, index):
         inputs = torch.zeros([3, 224, 224])
         label = 0
+        #get a single image
+
+        #load images with Pillow
+        #image = Image.open()
 
         return inputs, label
 
