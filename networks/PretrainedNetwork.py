27e9a30b9a6ffd62372ead55120b7a71cd1e3cf6
Arush Ramteke <arushramtek@gmail.com>
2022-02-16 21:36:03 -0800

    Scaled down pretrained network (helps with overfitting hopefully).

--- networks/PretrainedNetwork.py
+++ networks/PretrainedNetwork.py
@@ -16,9 +16,9 @@ class PretrainedNetwork(torch.nn.Module):
         self.prebase = models.resnet18(pretrained=True)
 
         self.fc1 = nn.Linear(1000, 1000)
-        self.fc2 = nn.Linear(1000, 1000)
-        self.fc3 = nn.Linear(1000, 100)
-        self.fc4 = nn.Linear(100,5)
+        self.fc2 = nn.Linear(1000, 500)
+        self.fc3 = nn.Linear(500, 80)
+        self.fc4 = nn.Linear(80,5)
         # self.sigmoid = nn.Sigmoid()
 
         #test
@@ -27,18 +27,8 @@ class PretrainedNetwork(torch.nn.Module):
         self.pool2 = nn.MaxPool2d(3,2)
         self.pool3 = nn.MaxPool2d(3,2)
 
-        #batch normalization
-        # self.bnl1 = nn.BatchNorm1d(4000)
-        # self.bnl2 = nn.BatchNorm1d(4000)
-
         self.bnl1 = nn.BatchNorm1d(1000)
         self.bnl2 = nn.BatchNorm1d(1000)
-
-        self.bnc1 = nn.BatchNorm2d(96)
-        self.bnc2 = nn.BatchNorm2d(256)
-        self.bnc3 = nn.BatchNorm2d(384)
-        self.bnc4 = nn.BatchNorm2d(384)
-        self.bnc5 = nn.BatchNorm2d(384)
 
         #dropout
         #self.drop1 = nn.Dropout(p=0.5)
