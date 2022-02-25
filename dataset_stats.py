da465776bdf915d6e61a19f67295fbd07bd6d170
Jai Parera <jaiparera@gmail.com>
2022-02-25 00:31:41 -0800

    Dataset stats mean

--- dataset_stats.py
+++ dataset_stats.py
@@ -1,28 +1,26 @@
+import torch
+from PIL import Image
+import torchvision.transforms as transforms
 import constants
-import numpy as np
-import statistics
-from PIL import Image 
 
 from data.StartingDataset import StartingDataset
-dataset = StartingDataset(csv_path='data/train.csv', folder_path='data/train_images', img_size=constants.IMG_SIZE)
+dataset = StartingDataset(csv_path='data/train.csv', folder_path='data/train_images', img_size=constants.IMG_SIZE[0])
 
-blue = []
-green = []
-red = []
-for i,id in enumerate(dataset.image_ids):
-    with Image.open(dataset.image_ids[i]) as image:
-        myimg = image.load()
-    avg_color_per_row = np.average(myimg, axis=0)
-    avg_color = np.average(avg_color_per_row, axis=0)/256
-    blue.append(avg_color[0])
-    green.append(avg_color[1])
-    red.append(avg_color[2])
+transform = transforms.ToTensor()
+# Read the input image
+imgTensor = torch.zeros(3,224,224)
+#temp = torch.zeros(3,224,224)
+c = 0
+for i,id in enumerate(dataset):
+    imgTensor += id[0]
+    c += 1
+    if c % 1000 == 0:
+        print(torch.mean(imgTensor, dim = [1,2]))
 
-print('Blue Mean', sum(blue)/len(blue))
-print('Blue std', statistics.pstdev(blue))
+# Compute mean of the Image Tensor across image channels RGB
+R_mean, G_mean ,B_mean = torch.mean(imgTensor, dim = [1,2])
 
-print('Green Mean', sum(green)/len(green))
-print('Green std', statistics.pstdev(green))
-
-print('Red Mean', sum(red)/len(red))
-print('Red std', statistics.pstdev(red))
+# print mean across image channel RGB
+print("Mean across Read channel:", R_mean/(21397))
+print("Mean across Green channel:", G_mean/(21397))
+print("Mean across Blue channel:", B_mean/(21397))
